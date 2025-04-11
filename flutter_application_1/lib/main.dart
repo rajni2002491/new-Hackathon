2a1d9c248f5c409ddf77275c1a260dd7544e27a5
bviswanath16 <bviswanath16@gmail.com>
2025-04-12 00:54:53 +0530

    updated firebase

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -3,6 +3,8 @@ import 'screens/splash_screen.dart';
 import 'screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 
 void main() {
